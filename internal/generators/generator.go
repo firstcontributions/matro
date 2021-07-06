a8c8e5a9e66e2144ae3b8c6ec01ca89ff15c3714
Gokul Thumbarathy Padmalochanan <tp.gokul@gmail.com>
2021-07-07 00:41:37 +0100

    added code documentation for generator factory and types

--- internal/generators/generator.go
+++ internal/generators/generator.go
@@ -8,18 +8,26 @@ import (
 	"github.com/firstcontributions/matro/internal/parser"
 )
 
+// Type defines a generator type
 type Type string
+
+// IGenerator is an interface with a generate function
 type IGenerator interface {
 	Generate() error
 }
 
 const (
-	TypeGQLSchema  Type = "graphql-schema"
-	TypeGQLServer  Type = "graphql-server"
-	TypeGRPCProto  Type = "grpc-proto"
+	// TypeGQLSchema defines the type for graphql schema generator
+	TypeGQLSchema Type = "graphql-schema"
+	// TypeGQLServer defines the type for graphql server code generator
+	TypeGQLServer Type = "graphql-server"
+	// TypeGRPCProto defines the type for gRPC protocol buffer generator
+	TypeGRPCProto Type = "grpc-proto"
+	// TypeMongoModel defines the type for mongo model code generator
 	TypeMongoModel Type = "mongo-model"
 )
 
+// GetGenerator is a factory method to get generator by given type
 func GetGenerator(t Type, path string, d *parser.Definition) IGenerator {
 	switch t {
 	case TypeGQLSchema:
