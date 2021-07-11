f0c9f65d2322d604a6d151e4acb191278366f246
Gokul Thumbarathy Padmalochanan <tp.gokul@gmail.com>
2021-07-11 23:36:19 +0100

    added more crud functions

--- internal/generators/models/mongo/templates.go
+++ internal/generators/models/mongo/templates.go
@@ -43,7 +43,7 @@ func (s *{{- title .Name -}}Store) getCollection (collection string) *mongo.Coll
 }
 `
 
-const modelTpl = `
+const crudTpl = `
 package mongo
 
 import (
@@ -54,6 +54,14 @@ import (
 
 
 )
+
+func (s *{{- title .Module -}}Store) Create{{- title .Name -}} (ctx context.Context, {{.Name}} *{{-  .Module -}}store. {{- title .Name}}) (* {{ .Module -}}store. {{- title .Name}}, error) {
+	
+	if _, err := s.getCollection(Collection{{title (plural .Name)}}).InsertOne(ctx, {{.Name}}); err != nil {
+		return nil, err
+	}
+	return {{ .Name}}, nil
+}
 
 func (s *{{- title .Module -}}Store) Get{{- title .Name -}}ByID (ctx context.Context, id string) (* {{ .Module -}}store. {{- title .Name}}, error) {
 	query := bson.M{
@@ -69,6 +77,31 @@ func (s *{{- title .Module -}}Store) Get{{- title .Name -}}ByID (ctx context.Con
 	return &{{- .Name}}, nil
 }
 
+func (s *{{- title .Module -}}Store) Get{{- title (plural .Name) -}} (
+	ctx context.Context,
+	{{- if not (empty .SearchFields)}}
+	search *string,
+	{{- end}}
+	{{- template "getargs" . }}
+	offset *string,
+	limit *int, 
+) (
+	[]*{{ .Module -}}store. {{- title .Name}}, 
+	error,
+) {
+	query := bson.M{}
+	{{- template "searchQuery" .}}
+
+	var {{plural .Name}} []*{{ .Module -}}store. {{- title .Name}}
+	if err := s.getCollection(Collection{{title (plural .Name)}}).Find(ctx, query).Decode(&{{- plural .Name -}}); err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &{{- plural .Name}}, nil
+}
+
 func (s *{{- title .Module -}}Store) Update{{- title .Name -}} (ctx context.Context, {{.Name}} *{{-  .Module -}}store. {{- title .Name}}) (* {{ .Module -}}store. {{- title .Name}}, error) {
 	query := bson.M{
 		"_id": {{.Name -}}.Id,
@@ -81,6 +114,37 @@ func (s *{{- title .Module -}}Store) Update{{- title .Name -}} (ctx context.Cont
 	}
 	return {{ .Name}}, nil
 }
+func (s *{{- title .Module -}}Store) Delete{{- title .Name -}}ByID (ctx context.Context, id string) (error) {
+	query := bson.M{
+		"_id": id,
+	}
+	if _,  err := s.getCollection(Collection{{title (plural .Name)}}).DeleteOne(ctx, query); err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, nil
+		}
+		return  err
+	}
+	return nil
+}
+
+{{- define "getargs"}}
+{{- $t := .}}
+{{- range .Filters}}
+	{{.}} *{{$t.FieldType .}},
+{{- end}}
+{{- end}}
+
+{{- define "searchQuery" }}
+{{- if not (empty .SearchFields)}}
+	searchQuery := map[string][]interface{}{
+		"$or": []interface{
+			{{- range .SearchFields }}
+			map[string]interface{}{"{{- . -}}": search},
+			{{- end}}
+		}
+	}
+{{- end}}
+{{- end}}
 `
 
 const modelTyp = `
